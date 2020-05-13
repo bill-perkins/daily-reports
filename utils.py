dbfee4d2cd5565fd96e2c3d2ef9f511bb587a657
Bill Perkins <bill.perkins@seacoastbank.com>
2020-05-13 10:59:33 -0400

    add some globals

--- utils.py
+++ utils.py
@@ -1,4 +1,14 @@
 # utility functions for this project
+from datetime import datetime
+from datetime import date
+from datetime import timedelta
+
+# if a line starts with a day of the week, we want to add the
+# hostname to the line.
+starters = [ 'Mon ', 'Tue ', 'Wed ', 'Thu ', 'Fri ', 'Sat ', 'Sun ' ]
+allSystems = {}     # Global dictionary as sysname: datedEntries
+
+oneday = timedelta(days = 1) # Global timedelta of one day
 
 # ----------------------------------------------------------------------------
 # getContent(filename)

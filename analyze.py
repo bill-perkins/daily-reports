a85795164a8dfddcbbd4dcecf87d3b4770c017bf
Bill Perkins <bill.perkins@seacoastbank.com>
2020-05-13 16:12:02 -0400

    invariant disks retain all values

--- analyze.py
+++ analyze.py
@@ -16,30 +16,32 @@ def analyze(systems):
     """
 
     global oneday
+    variance = 19.9
 
-    # invariants dictionary
-    invariants = {'/':   0.0, \
-            '/opt/sas':  0.0, \
-            '/sasdata':  0.0, \
-            '/sastmp':   0.0, \
-            'Mem':       0.0, \
-            'Swap:':     0.0, \
-            'ping test': '',  \
-            'services':  '',  \
-            'Uptime':   '' }
-
-    # --- analyze some data:
+    # --- analyze each system:
     for sysname, datedEntries in list(systems.items()):
         nexttime = date(2020,1,3)
 
         print('Analyzing system', sysname + ':')
         print()
 
+        # invariants dictionary, start fresh for each system:
+        invariants = {'/':   [0.0, 0.0, 0,0, 0,0], \
+                '/opt/sas':  [0.0, 0.0, 0,0, 0,0], \
+                '/sasdata':  [0.0, 0.0, 0,0, 0,0], \
+                '/sastmp':   [0.0, 0.0, 0,0, 0,0], \
+                'Mem':       [0.0, 0.0, 0,0, 0,0], \
+                'Swap:':     [0.0, 0.0, 0,0, 0,0], \
+                'ping test': '',  \
+                'services':  '',  \
+                'Uptime':   '' }
+
         # grab logs in date order:
         entry_dates = sorted(datedEntries)
         if len(entry_dates[0]) == 0:
             entry_dates.pop(0) # get rid of that annoying blank entry at the start
 
+        # analyze each dated entry:
         for datestamp in entry_dates:
             # entry is the log dictionary for this datestamp:
             entry = datedEntries[datestamp]
@@ -57,14 +59,12 @@ def analyze(systems):
                     nexttime = thisdate
                     print(thisdate, 'Logging starts')
 
-            # create a date object for the next day:
+            # set nexttime to thisdate plus 1 day:
             nexttime = thisdate + oneday
 
-            """
-                Search for changes to invariant data.
-                When something comes up different, complain about it,
-                then change the invariants list to the new value.
-            """
+            # Search for changes to invariant data.
+            # When something comes up different, complain about it,
+            # then change the invariants list to the new value.
             for key, value in list(invariants.items()):
                 try:
                     val = entry[key]
@@ -109,6 +109,13 @@ def analyze(systems):
                     print()
                     continue
 
+                # see if the key is one of the disks:
+                if key in list(invariants)[0:4]:
+                    if value == '' or value[0] == 0.0:
+                        invariants[key] = val # set 'was' values
+                    else:
+                        chk_variance(invariants[key], value, variance)
+
                 else:   # we're looking at a different invariant key
                     if value == '' or value == 0.0:
                         pass
@@ -116,6 +123,7 @@ def analyze(systems):
                         print(thisdate, key + \
                                 ": expected: '" + str(value) + \
                                 "', found: '" + str(val[0]) + "'")
+
                     invariants[key] = val[0]
                     continue
 

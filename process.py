40e4def6d13586b70194cb2df198042ddc79c818
Bill Perkins <bill.perkins@seacoastbank.com>
2020-05-15 15:28:31 -0400

    use a deque for inputdata more docs simplify variable names better output messages

--- process.py
+++ process.py
@@ -1,6 +1,7 @@
 # process.py
 #
 import sys
+from collections import deque
 
 import lclvars
 
@@ -14,7 +15,8 @@ def getContent(filename):
     """bring the contents of the given input file into a list,
        and return the list.
        filename: name of the file we're processing (daily.log)
-       returns:  list of lines from given file
+       returns:  deque of lines from given filename;
+                exits program if file not found or permissions error.
     """
 
     logcontent = []
@@ -26,11 +28,11 @@ def getContent(filename):
 
     except FileNotFoundError as err:
         print('getContent():', str(err))
-        sys.exit(0)
+        sys.exit(1)
 
     except PermissionError as err:
         print('getContent():', str(err))
-        sys.exit(0)
+        sys.exit(1)
 
     return deque(logcontent)
 
@@ -38,26 +40,22 @@ def getContent(filename):
 # getEntry(inpdata)
 # ----------------------------------------------------------------------------
 def getEntry(inpdata):
-    """ Bring in lines from current index, until we see '----'.
-            inpdata:  list of lines from getContent()
-            index:    line # we start processing at
-            maxindex: last line index of inpdata
+    """ Bring in lines from deque until we see '----'.
+            inpdata:  deque of lines from getContent()
 
         returns:
             outlist:  list of lines in this entry
     """
 
     outlist = []
-    while True:
+
+    line = inpdata.popleft()
+    while line[0:4] != '----':
+        outlist.append(line)
         try:
             line = inpdata.popleft()
         except IndexError as err:
             return None
-
-        if line[0:4] == '----':
-            break;
-
-        outlist.append(line)
 
     return outlist
 
@@ -69,9 +67,8 @@ def process(logfile):
         along with all the dated entries in a dictionary
     """
 
-    print('processing:', logfile)
+    print('processing log file:', logfile)
 
-    ix = 0              # current index into inp_file
     cur_syskey = ''     # current system name
     inp_entry = []      # create local entry list
     out_entry = []      # create local entry list
@@ -79,8 +76,7 @@ def process(logfile):
     curSystime = ''     # system time of this entry
 
     inp_file = getContent(logfile)  # bring file into inp_file
-    inp_max  = len(inp_file)        # lines in file
-    if inp_max == 0:
+    if len(inp_file) == 0:
         print('process(): empty file', logfile)
         return None, None
 
@@ -91,21 +87,18 @@ def process(logfile):
             break;
 
     if cur_syskey == '':
-        print("can't find system hostname in", logfile)
+        print("can't find hostname in", logfile)
         print('...skipping...')
         print()
         return None, None
 
     lclvars.curSysname = cur_syskey
 
-#    while ix < inp_max:
     while True:
-
         # declare logEntries here so we always have a fresh one:
         logEntries = {}  # dictionary with log parameter as key, value as value
 
         # read in a full log entry:
-#        ix, inp_entry = getEntry(inp_file, ix, inp_max)
         inp_entry = getEntry(inp_file)
         if inp_entry == None:
             break;
@@ -117,22 +110,22 @@ def process(logfile):
         # handle Datestring, Datestamp, and Sysname special:
         dateKey = ''    # fresh dateKey
         for x in out_entry:
-            thisKey = x[0]
-            thisVal = x[1:]
+            key = x[0]
+            val = x[1:]
 
-            if 'Datestamp' in thisKey:
-                dateKey = thisVal[0]
-            elif 'Systime' in thisKey:
-                curSystime = thisKey
-            elif 'Sysname' in thisKey:
-                if thisVal[0] != lclvars.curSysname:
-                    print("Foreign system name in input file: '" + thisVal[0] + "'")
-                    print("thisVal[0] =", thisVal[0], "lclvars.curSysname =", lclvars.curSysname)
+            if 'Datestamp' in key:
+                dateKey = val[0]
+            elif 'Systime' in key:
+                curSystime = key
+            elif 'Sysname' in key:
+                if val[0] != lclvars.curSysname:
+                    print("Foreign system name in input file: '" + val[0] + "'")
+                    print("val[0] =", val[0], "lclvars.curSysname =", lclvars.curSysname)
                     sys.exit(1)
-            elif 'Datetime' in thisKey:
-                logEntries[thisKey] = thisVal
+            elif 'Datetime' in key:
+                logEntries[key] = val
             else:
-                logEntries[thisKey] = thisVal
+                logEntries[key] = val
 
         # create datedEntries using the datestamp for its keys:
         datedEntries[dateKey] = logEntries

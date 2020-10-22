7a8974baaf39728916bb42d1882d3159acb29c43
Bill Perkins <bill.perkins@seacoastbank.com>
2020-10-22 14:48:58 -0400

    initial commit

---
+++ lclvars.py
@@ -0,0 +1,11 @@
+# variables for reports project:
+from datetime import datetime
+from datetime import date
+from datetime import timedelta
+
+# if a line starts with a day of the week, we want to add the
+# hostname to the line.
+starters = [ 'Mon ', 'Tue ', 'Wed ', 'Thu ', 'Fri ', 'Sat ', 'Sun ' ]
+oneday = timedelta(days = 1) # Global timedelta of one day
+
+# EOF:
